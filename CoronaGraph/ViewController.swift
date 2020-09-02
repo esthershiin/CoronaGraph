28e15ce50a707707e60ccc6b2e7b851457e2a98a
Esther Shin <esthershiin@berkeley.edu>
2020-09-02 02:37:34 -0700

    initialized proj

---
+++ CoronaGraph/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaGraph
+//
+//  Created by Esther Shin on 9/2/20.
+//  Copyright © 2020 Esther Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
